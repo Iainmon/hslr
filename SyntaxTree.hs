34cd37eda9270eeea824dda251d67853f6371b88
Iain Moncrief <Iainmoncrief@gmail.com>
2021-03-09 23:19:46 -1000

    made language representation interface base

--- SyntaxTree.hs
+++ SyntaxTree.hs
@@ -1,13 +1,36 @@
+{-# LANGUAGE  FlexibleInstances #-}
 
 data Type = Int | Float | Bool | Vector2 | Vector3 | Vector4 deriving (Eq)
 
 data CallType = Unary (SyntaxTree) | Binary (SyntaxTree,SyntaxTree) | Trinary (SyntaxTree,SyntaxTree,SyntaxTree) deriving (Eq)
-data AST = Call String CallType
-    | Id String deriving (Eq)
+data AST = Call String CallType | Id String deriving (Eq)
+
 data SyntaxTree = 
      Node Type AST
     | Imparitive [SyntaxTree] SyntaxTree
     | Assignment Type String SyntaxTree deriving (Eq)
+
+class GLiteral t where
+    represent :: t -> String
+    syntaxNode :: t -> SyntaxTree
+
+instance GLiteral Float where
+    represent = show
+    syntaxNode n = Node Float $ Id $ represent n
+instance GLiteral Double where
+    represent = show
+    syntaxNode n = Node Float $ Id $ represent n
+instance GLiteral Int where
+    represent = show
+    syntaxNode n = Node Int $ Id $ represent n
+instance GLiteral Bool where
+    represent True = "true"
+    represent False = "false"
+    syntaxNode var = Node Bool $ Id $ represent var
+instance (Num a, Num b, GLiteral a, GLiteral b) => GLiteral (a,b) where
+    represent = show . syntaxNode
+    syntaxNode (a,b) = Node Vector2 $ Call "vec2" $ Binary (syntaxNode a, syntaxNode b)
+    
 
 
 instance Show Type where
@@ -46,4 +69,19 @@ cosine :: SyntaxTree -> SyntaxTree
 cosine (Node Float x) = Node Float $ Call "cos" $ Unary (Node Float x)
 cosine _ = undefined
 
-ast = Node Float (Id "iain")
+wrap :: (GLiteral a) => (SyntaxTree -> SyntaxTree) -> a -> SyntaxTree
+wrap f = f . syntaxNode
+
+
+
+var :: Type -> String -> SyntaxTree
+var ty name = Node ty $ Id name
+
+iden :: String -> AST
+iden s = Id s
+
+-- vector2 :: SyntaxTree -> SyntaxTree -> SyntaxTree
+-- vector2 (Node Float x) (Node Float y) = Node Vector2 $ Call "vec2" $ Binary (Node Float x) (Node Float y)
+
+ast = var Float "iain"
+
